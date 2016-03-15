8ec017d04eee53bcbbdc651a434cbe1b04efc49b
rswa <rswa000@gmail.com>
2016-03-15 17:17:09 +0800

    update website

---
+++ index(var).php
@@ -0,0 +1,3 @@
+<?php
+$OESiteUploadDirectory = "Upload";
+$OEConfWESendMail = '{"WE9f1ed06c06":{"ScriptVersion":29,"ConfSendMail":{"IsUpdated":false,"Type":3,"TypeStr":"SMTP","SpecialType":"","SMTPHost":null,"SMTPPort":25,"SMTPUser":null,"SMTPPassWrd":null,"SMTPSSL":false},"FormLinks":{"ElementsID":{},"ButtonSubmitID":null,"ButtonCancelID":null},"SendMailInfo":{"AdminEmail":{"Items":{"DEFAULT":""}},"CopycacheEmail":{"Items":{"DEFAULT":""}},"CopyEmail":{"Items":{"DEFAULT":""}},"ReturnEmail":null,"ReturnEmailID":null,"Subject":null,"SendReceipt":false,"ReceiptHeader":{"Items":{"DEFAULT":""}},"ReceiptBody":{"Items":{"DEFAULT":""}},"SendReceiptSignature":false,"ReceiptSignature":{"Target":null,"Links":{"Items":{"DEFAULT":""}}},"UserEmailID":null,"ListInputTitle":{}}}}';
